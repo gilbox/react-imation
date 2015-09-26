24342d6421b3a87a03f5eee318089ea90124318e
gilbox <birmangil@gmail.com>
2015-09-25 20:11:41 -0700

    demo5

---
+++ examples/demo5/Demo5.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {Timeline, tween, ease} from 'react-imation';
+import {Easer} from 'functional-easing';
+
+const easeIn = ease(new Easer().using('in-cubic'));
+const easeOut = ease(new Easer().using('out-cubic'));
+
+const ballDiameter = 50;
+
+const containerStyle = {
+  position: 'relative',
+  width: '100vw',
+  height: '100vh',
+};
+
+const ballContainerStyle = {
+  position: 'absolute',
+  left: '50%',
+  top: 'calc(100vh - 450px)',
+  width: ballDiameter,
+  transform: 'translate(-50%, -50%)'
+}
+
+export default class Demo5 extends Component {
+  render() {
+    return (
+      <div style={containerStyle}>
+        <div style={ballContainerStyle}>
+          <Timeline loop={true} min={0} max={100} playOnMount={true}>
+          {({tween:twn}) => {
+
+            const y = twn({
+              0:   easeIn(0),
+              50:  easeOut(400),
+              100: 0 });
+
+            return <div
+              style={{
+                position: 'absolute',
+                borderRadius: '50%',
+                background: 'red',
+                width: ballDiameter,
+                height: ballDiameter + tween(y, {0:0, 370:0, 400:-40}),
+                transform: `translateY(${y}px)`
+              }} />
+          }}</Timeline>
+        </div>
+      </div>
+    )
+  }
+}
