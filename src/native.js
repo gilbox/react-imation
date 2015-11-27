05c6e1e768b8db78fb00ee533d67f9e883a9284d
gilbox <birmangil@gmail.com>
2015-11-27 00:20:11 -0800

    remove timeline from main rn import

--- src/native.js
+++ src/native.js
@@ -1,6 +1,5 @@
 // not using export * because react-native is weird
 
-import * as timeline from './timeline/native';
 import * as tween from './tween';
 
-export default {...tween, ...timeline};
+export default tween;
